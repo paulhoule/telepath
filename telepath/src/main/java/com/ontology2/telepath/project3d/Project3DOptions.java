01630db8be56b8e77c72df6ce3537055d9fa8884
Paul Houle <paul@ontology2.com>
2014-01-04 16:41:39 -0500

    plausible Tool and Options for Project3D ready for integraton test w/ AWS

---
+++ telepath/src/main/java/com/ontology2/telepath/project3d/Project3DOptions.java
@@ -0,0 +1,38 @@
+package com.ontology2.telepath.project3d;
+
+import com.ontology2.centipede.parser.ContextualConverter;
+import com.ontology2.centipede.parser.HasOptions;
+import com.ontology2.centipede.parser.Option;
+import org.apache.hadoop.fs.Path;
+
+import java.util.List;
+
+public class Project3DOptions implements HasOptions {
+    @Option(name="R",description="number of reducers")
+    public int reducerCount;
+
+    @Option(description="input and output file default directory")
+    public String dir;
+
+    @Option(description="input files",contextualConverter=PathConverter.class)
+    public List<String> input;
+
+    @Option(description="output files",contextualConverter=PathConverter.class)
+    public String output;
+
+    public static class PathConverter implements ContextualConverter<String> {
+        public String convert(String value, HasOptions that) {
+            String defaultDir=getDefaultDir((Project3DOptions) that);
+
+            if(defaultDir.isEmpty())
+                return value;
+
+            Path there=new Path(defaultDir,value);
+            return there.toString();
+        }
+
+        public String getDefaultDir(Project3DOptions that) {
+            return that.dir;
+        }
+    }
+}
---
+++ telepath/src/main/java/com/ontology2/telepath/project3d/Project3DTool.java
@@ -0,0 +1,70 @@
+package com.ontology2.telepath.project3d;
+
+import com.google.common.collect.Lists;
+import com.ontology2.bakemono.configuration.HadoopTool;
+import com.ontology2.bakemono.entityCentric.EntityCentricMapper;
+import com.ontology2.bakemono.entityCentric.EntityIsAReducer;
+import com.ontology2.bakemono.entityCentric.ExtractIsAOptions;
+import com.ontology2.bakemono.mapred.ToolBase;
+import com.ontology2.centipede.parser.OptionParser;
+import com.ontology2.centipede.shell.UsageException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+
+import java.util.List;
+
+@HadoopTool("project3D")
+public class Project3DTool extends ToolBase {
+    @Override
+    public int run(String[] strings) throws Exception {
+        Project3DOptions options = extractOptions(Lists.newArrayList(strings));
+        configureOutputCompression();
+        Job job=new Job(getConf(),"project3D");
+        job.setJarByClass(this.getClass());
+        job.setMapperClass(Project3DMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        job.setReducerClass(Project3DReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        for(String path: options.input) {
+            FileInputFormat.addInputPath(job, new Path(path));
+        }
+
+
+        job.setNumReduceTasks(options.reducerCount);
+        FileOutputFormat.setOutputPath(job, new Path(options.output));
+        FileOutputFormat.setCompressOutput(job,true);
+        FileOutputFormat.setOutputCompressorClass(job,GzipCodec.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    Project3DOptions extractOptions(List<String> strings) throws IllegalAccessException {
+        OptionParser parser=new OptionParser(ExtractIsAOptions.class);
+        applicationContext.getAutowireCapableBeanFactory().autowireBean(parser);
+
+        Project3DOptions options=(Project3DOptions) parser.parse(Lists.newArrayList(strings));
+        if (options.input.isEmpty())
+            throw new UsageException("You did not specify a value for -input");
+
+        if (options.output==null || options.output.isEmpty())
+            throw new UsageException("You did not specify a value for -output");
+
+        if(options.reducerCount<1) {
+            options.reducerCount=1;
+        }
+        return options;
+    }
+
+}
