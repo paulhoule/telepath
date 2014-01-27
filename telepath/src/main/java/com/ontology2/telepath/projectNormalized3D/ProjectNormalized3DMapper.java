8b9d7e3f3db56bdabd626e49e36f6570a7666553
Paul Houle <paul@ontology2.com>
2014-01-27 16:08:25 -0500

    issue #3 -- lots of unit test for an implementation that might pass integration testing.

--- telepath/src/main/java/com/ontology2/telepath/projectNormalized3D/ProjectNormalized3DMapper.java
+++ telepath/src/main/java/com/ontology2/telepath/projectNormalized3D/ProjectNormalized3DMapper.java
@@ -2,16 +2,27 @@ package com.ontology2.telepath.projectNormalized3D;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import static com.google.common.collect.Maps.*;
 import static com.ontology2.bakemono.util.Utilities.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -22,6 +33,50 @@ public class ProjectNormalized3DMapper extends Mapper<LongWritable,Text,Text,Flo
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         super.setup(context);
+        loadNormalizationFactors(context);
+    }
+
+    void loadNormalizationFactors(Context context) throws IOException {
+        String systemPath=context.getConfiguration().get(ProjectNormalized3DTool.FACTORS);
+        if(systemPath==null)
+            return;
+
+        loadNormalizationFactors(context, new Path(systemPath));
+    }
+
+    void loadNormalizationFactors(Context context,Path path) throws IOException {
+        FileSystem fs=FileSystem.get(path.toUri(), context.getConfiguration());
+        InputStream in=fs.open(path);
+        loadNormalizationFactors(in,getYrmo(context));
+    }
+
+    String getYrmo(Context context) {
+        FileSplit split=(FileSplit) context.getInputSplit();
+        String name=split.getPath().toString();
+        List<String> parts=SLASH_SPLITTER.splitToList(name);
+        return parts.get(parts.size()-2);
+    }
+
+    void loadNormalizationFactors(InputStream in, String thisYrmo) throws IOException {
+        thisYrmo=thisYrmo.intern();
+        BufferedReader r=new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+        while(true) {
+            String line=r.readLine();
+            if(line==null)
+                break;
+
+            if(line.startsWith("#"))
+                continue;
+
+            Iterator<String> parts=WHITESPACE_SPLITTER.split(line).iterator();
+            String yrmo=parts.next().intern();
+            if(yrmo==thisYrmo) {
+                String site=parts.next();
+                int cntURIs=Integer.parseInt(parts.next());
+                float cntHits=(float) Long.parseLong(parts.next());
+                siteNormalizationFactor.put(site,cntHits);
+            }
+        }
     }
 
     @Override
--- telepath/src/test/java/com/ontology2/telepath/projectNormalized3D/TestMapper.java
+++ telepath/src/test/java/com/ontology2/telepath/projectNormalized3D/TestMapper.java
@@ -1,19 +1,32 @@
 package com.ontology2.telepath.projectNormalized3D;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import java.io.InputStream;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("../applicationContext.xml")
 public class TestMapper {
     ProjectNormalized3DMapper mapper;
     Mapper<LongWritable,Text,Text,FloatWritable>.Context context;
+    @Autowired ApplicationContext applicationContext;
 
     @Before
     public void setup() {
@@ -22,6 +35,9 @@ public class TestMapper {
         mapper.siteNormalizationFactor.put("emhd",18.10F);
         mapper.siteNormalizationFactor.put("nflx",386.08F);
         context=mock(Mapper.Context.class);
+        FileSplit split=mock(FileSplit.class);
+        stub(split.getPath()).toReturn(new Path("/funky/2008-01/pagecount-blah200901831234.gz"));
+        stub(context.getInputSplit()).toReturn(split);
     }
 
     @Test
@@ -37,5 +53,20 @@ public class TestMapper {
         mapper.map(new LongWritable(10),in,context);
         verify(context).write(new Text("xiv ZMQRZ"),new FloatWritable(7/32.33F));
         verifyNoMoreInteractions(context);
+    }
+
+    @Test
+    public void loadNormalizationFactorsFromResource() throws IOException {
+        mapper.siteNormalizationFactor.clear();
+        InputStream in=applicationContext.getResource("com/ontology2/telepath/projectNormalized3D/normalizationSample.txt").getInputStream();
+        mapper.loadNormalizationFactors(in,"2008-01");
+        assertEquals(2,mapper.siteNormalizationFactor.size());
+        assertEquals(1024.0F,mapper.siteNormalizationFactor.get("ba"),0.001F);
+        assertEquals(99.0F,mapper.siteNormalizationFactor.get("be"),0.001F);
+    }
+
+    @Test
+    public void getsYrmo() {
+        assertEquals("2008-01",mapper.getYrmo(context));
     }
 }
