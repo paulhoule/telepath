5112a30ff4d9865f130a2911e5d0c3efebed096f
Paul Houle <paul@ontology2.com>
2014-02-04 12:47:58 -0500

    add INFO level logging to MergeReducer so we can see all data going through.  (will run this against a tiny data set)

--- telepath/src/main/java/com/ontology2/telepath/mergeAgainstDBpedia/MergeReducer.java
+++ telepath/src/main/java/com/ontology2/telepath/mergeAgainstDBpedia/MergeReducer.java
@@ -1,17 +1,24 @@
 package com.ontology2.telepath.mergeAgainstDBpedia;
 
+import com.google.common.base.Splitter;
 import com.ontology2.bakemono.joins.TaggedTextItem;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
 public class MergeReducer extends Reducer<TaggedTextItem,TaggedTextItem,Text,Text> {
+    Log LOG= LogFactory.getLog(MergeReducer.class);
 
     @Override
     protected void reduce(TaggedTextItem key, Iterable<TaggedTextItem> values, Context context) throws IOException, InterruptedException {
         Text toValue=null;
+
+        LOG.info("Processed key ["+key.getKey()+"] with tag ["+key.getTag().get()+"]");
         for(TaggedTextItem value:values) {
+            LOG.info("Processed value ["+value.getKey()+"] with tag ["+value.getTag().get()+"]");
             int tag=value.getTag().get();
             switch(tag) {
                 case 1:
