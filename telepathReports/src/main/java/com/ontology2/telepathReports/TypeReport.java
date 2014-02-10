1bb835b71e15b7488f14bcdb6277f3ee2267eefe
Paul Houle <paul@ontology2.com>
2014-02-10 15:23:02 -0500

    basic interpolation into FreeMarker

--- telepathReports/src/main/java/com/ontology2/telepathReports/TypeReport.java
+++ telepathReports/src/main/java/com/ontology2/telepathReports/TypeReport.java
@@ -2,11 +2,19 @@ package com.ontology2.telepathReports;
 
 import com.hp.hpl.jena.query.*;
 import com.ontology2.centipede.shell.CommandLineApplication;
+import freemarker.template.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Maps.newHashMap;
 
 
 @Component("typeReport")
@@ -16,22 +24,33 @@ public class TypeReport extends CommandLineApplication {
 
     @Override
     protected void _run(String[] strings) throws Exception {
-
         Query query=sparqlService.getQuery("/com/ontology2/telepathReports/typeReport.sparql");
         ResultSet results=sparqlService.select(query);
+
+        List<Map<String,Object>> rowz=newArrayList();
         while(results.hasNext()) {
             QuerySolution row=results.next();
-            StringBuffer out=new StringBuffer();
             Iterator<String> names=row.varNames();
-            int cnt=0;
+            Map<String,Object> rowOut=newHashMap();
+            rowz.add(rowOut);
             while(names.hasNext()) {
-                if(cnt>0)
-                    out.append(",");
-
-                out.append(row.get(names.next()));
-                cnt++;
+                String name=names.next();
+                rowOut.put(name, row.get(name));
             }
-            System.out.println(out);
         }
+
+        Map<String,Object> root=newHashMap();
+        root.put("rows",rowz);
+        Configuration c=new Configuration();
+        c.setClassForTemplateLoading(SparqlService.class,"/com/ontology2/telepathReports");
+        c.setObjectWrapper(new DefaultObjectWrapper());
+        c.setDefaultEncoding("UTF-8");
+        c.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+        c.setIncompatibleImprovements(new Version(2,3,20));
+
+        Template t=c.getTemplate("typeReport.ftl");
+        Writer out=new OutputStreamWriter(System.out);
+        t.process(root,out);
     }
+
 }
---
+++ telepathReports/src/main/resources/com/ontology2/telepathReports/typeReport.ftl
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Alive and Kicking</title>
+ </head>
+ <body>
+  <table>
+    <#list rows as row >
+    <tr><td>${row.type}</td><td>${row.sum}</td><td>{$row.cnt}</td></tr>
+    </#list>
+  <table>
+ </body>
+</html>
